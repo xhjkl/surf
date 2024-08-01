eeaca24febd36e4635f7ee938662d42d3c32032e
Denis Makarenko <xhjkl@users.noreply.github.com>
2024-08-01 17:15:03 +0400

    pretty

--- src/record.rs
+++ src/record.rs
@@ -2,7 +2,7 @@
 
 use solana_sdk::{pubkey::Pubkey, signature::Signature};
 
-#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, serde::Deserialize)]
 pub struct Vote {
     pub signature: Signature,
     pub block_index: u64,
@@ -11,7 +11,7 @@ pub struct Vote {
     pub target: Pubkey,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, serde::Deserialize)]
 pub struct Transfer {
     pub signature: Signature,
     pub block_index: u64,
@@ -26,4 +26,40 @@ pub struct Transfer {
 pub enum Record {
     Vote(Vote),
     Transfer(Transfer),
+}
+
+// Forcing the keys to be pretty.
+impl serde::Serialize for Vote {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeMap;
+
+        let mut map = serializer.serialize_map(Some(5))?;
+        map.serialize_entry("signature", &self.signature.to_string())?;
+        map.serialize_entry("block", &self.block_index)?;
+        map.serialize_entry("timestamp", &self.timestamp)?;
+        map.serialize_entry("author", &self.author.to_string())?;
+        map.serialize_entry("target", &self.target.to_string())?;
+        map.end()
+    }
+}
+
+impl serde::Serialize for Transfer {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeMap;
+
+        let mut map = serializer.serialize_map(Some(5))?;
+        map.serialize_entry("signature", &self.signature.to_string())?;
+        map.serialize_entry("block", &self.block_index)?;
+        map.serialize_entry("timestamp", &self.timestamp)?;
+        map.serialize_entry("source", &self.source.to_string())?;
+        map.serialize_entry("destination", &self.destination.to_string())?;
+        map.serialize_entry("lamports", &self.lamports)?;
+        map.end()
+    }
 }
